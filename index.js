db643f265e9758fd5955370a836add8d6a091e04
HuahuaWang2017 <wanghuahua2016@gmail.com>
2018-06-04 20:33:20 -0700

    change

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-console.log('Current Branch: Branch_YangYang');
 
+//show the repository when clik the repository button
+//*******************************************************
+var repo = document.getElementById('repository');
 
+function ShowRepoModule ()
+{
+  var show3 = document.getElementsByClassName('hidden3');
+  for(var i=0;i<show3.length;i+=1)
+  {
+    show3[i].style.display = 'block';
+  }
+}
+
+repo.addEventListener('click',ShowRepoModule);
+//********************************************************
+
+var AddCash = document.getElementsByClassName('cash');
